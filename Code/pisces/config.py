7b1cc7e6dd0294afbac3611384778ff91cc3adb5
Leo Cui <leo_cui@hms.harvard.edu>
2024-12-01 19:33:30 -0500

    Created configure files

---
+++ Code/pisces/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+HOMEDIR = Path(__file__).resolve().parents[2]
+DATADIR = HOMEDIR / 'Data'
+OUTDIR = HOMEDIR / 'Results'
---
+++ configure.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+json_file="config.json"
+
+if [[ -n "$SLURM_JOB_ID" || -n "$PBS_JOBID" || -d "/etc/slurm" ]]; then
+  module load python/3.10.11
+  echo "Python module loaded"
+fi
+
+if [ -f "$json_file" ]; then
+  rm "$json_file"
+fi
+
+current_folder=$(pwd)
+
+cat <<EOF >"$json_file"
+{
+  "home_folder": "$current_folder"
+}
+EOF
+
+echo "config file setted up!"
+mkdir -p Data
+mkdir -p Results
+
+if [ -d "pisces_env" ]; then
+  source pisces_env/bin/activate
+  echo "Virtual environment activated"
+else
+  echo "Warning: Virtual environment 'pisces_env' not found."
+  python3 -m venv pisces_env
+  echo "Virtual environment created."
+fi
+
+if [ -f "requirements.txt" ]; then
+  echo "requirements.txt found. Installing dependencies..."
+  pip install -r requirements.txt
+  echo "Dependencies installed successfully."
+
+else
+  echo "Error: requirements.txt not found in the current directory."
+  exit 1
+fi
